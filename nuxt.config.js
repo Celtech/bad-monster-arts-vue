a04452b3e4f2eef2483ccbe500462017af566f2a
Tim Hinz <tim.hinz@guns.com>
2020-02-04 13:30:51 -0600

    updates port for production

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
       cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt'))
-    }
+    },
+    port: (process.env.NODE_ENV === 'production') ? 80 : 3000
   },
 
   mode: 'universal',
