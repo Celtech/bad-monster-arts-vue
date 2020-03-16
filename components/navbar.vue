fc8430e9079714e8d7f255e029d2a9921bf83b75
Tim Hinz <tim.hinz@guns.com>
2020-03-16 18:09:59 -0500

    Updated how the logo is drawn

--- components/navbar.vue
+++ components/navbar.vue
@@ -11,13 +11,13 @@
           <nuxt-link to="/">
             <div class="logo-wrapper">
               <svg
-                class="logo"
                 viewBox="0 0 256 75"
                 xmlns="http://www.w3.org/2000/svg"
                 role="img"
               >
                 <image
-                  class="logo"
+                  width="100%"
+                  height="100%"
                   xlink:href="https://cdn.badmonsterarts.com/main_logo.svg"
                   src="https://cdn.badmonsterarts.com/main_logo.png"
                 />
@@ -106,11 +106,6 @@
       width: 256px;
       height: 75px;
       overflow: hidden;
-
-      .logo {
-        width: auto;
-        height: 100%;
-      }
     }
 
     .navbar-nav {
