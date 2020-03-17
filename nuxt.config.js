1200af711ba6aa7e88361f9dc858c9ca34844d80
Tim Hinz <tim.hinz@guns.com>
2020-03-17 11:58:52 -0500

    Updated OG:Img url

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,7 +86,7 @@ export default {
         title: 'Bad Monster Arts | Indie Game Studio',
         description:
           'Indie Game studio with a passion for building beautiful games.',
-        img: 'ogimg.jpg',
+        img: 'https://cdn.badmonsterarts.com/ogimg.jpg',
         locale: 'en_US',
         twitter: '@BadMonsterArts',
         themeColor: '#31355e'
