49fbf4d67f5975865461221d69e54a00f1a58818
Tim Hinz <tim.hinz@guns.com>
2020-03-17 13:46:38 -0500

    Update to routes

---
+++ pages/post/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexVue',
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style scoped></style>
