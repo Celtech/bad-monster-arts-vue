1de6d0b7aa8853e18f08cc442da0725fd010c8f1
Tim Hinz <tim.hinz@guns.com>
2020-07-17 15:46:48 -0500

    About us content fillerr

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,87 @@
 <template>
   <article>
     <h1>About Bad Monster Arts</h1>
+    <h5>Just some filler text, this isn't a real company.</h5>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Lorem mollis aliquam
+      ut porttitor leo a diam sollicitudin. Pulvinar proin gravida hendrerit
+      lectus.
+
+      <img
+        src="https://www.vrlife.news/wp-content/uploads/2016/05/indie-game-characters.jpg"
+        alt="team photo"
+        style="float: left; width: 500px; margin-right: 5px;"
+      />
+
+      Etiam dignissim diam quis enim lobortis scelerisque. Dignissim convallis
+      aenean et tortor. Platea dictumst quisque sagittis purus sit amet
+      volutpat. Hac habitasse platea dictumst vestibulum rhoncus est. Laoreet
+      non curabitur gravida arcu ac. Iaculis urna id volutpat lacus laoreet non
+      curabitur gravida arcu. Massa vitae tortor condimentum lacinia quis vel
+      eros donec. Vulputate odio ut enim blandit volutpat maecenas volutpat.
+    </p>
+    <p>
+      Donec adipiscing tristique risus nec feugiat in fermentum posuere. Nulla
+      facilisi cras fermentum odio eu feugiat pretium nibh. Etiam erat velit
+      scelerisque in dictum. Nibh praesent tristique magna sit amet purus
+      gravida quis blandit. Amet tellus cras adipiscing enim eu turpis egestas
+      pretium aenean. Neque gravida in fermentum et sollicitudin. Eget dolor
+      morbi non arcu risus.
+      <img
+        src="https://mk0knowtechie1qof48y.kinstacdn.com/wp-content/uploads/2016/06/game-developers.jpg"
+        alt="team photo"
+        style="float: right; width: 500px"
+      />
+      Ipsum suspendisse ultrices gravida dictum fusce. Diam sollicitudin tempor
+      id eu. Mattis vulputate enim nulla aliquet porttitor lacus luctus.
+      Fringilla urna porttitor rhoncus dolor purus non enim praesent. Pharetra
+      massa massa ultricies mi quis hendrerit dolor. Quis risus sed vulputate
+      odio ut enim blandit volutpat. In arcu cursus euismod quis viverra nibh.
+    </p>
+    <p>
+      Et netus et malesuada fames ac turpis egestas sed. Suspendisse faucibus
+      interdum posuere lorem. Ultrices eros in cursus turpis massa tincidunt dui
+      ut. Viverra tellus in hac habitasse platea dictumst vestibulum. Tincidunt
+      eget nullam non nisi est sit amet. Ac felis donec et odio pellentesque
+      diam volutpat commodo sed. Urna et pharetra pharetra massa massa ultricies
+      mi quis. Metus vulputate eu scelerisque felis imperdiet proin fermentum.
+      Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Non
+      consectetur a erat nam. Eget magna fermentum iaculis eu non diam phasellus
+      vestibulum lorem. Integer vitae justo eget magna fermentum. Eu ultrices
+      vitae auctor eu augue. Ante metus dictum at tempor commodo ullamcorper a
+      lacus. Amet est placerat in egestas erat. Lacus sed turpis tincidunt id
+      aliquet risus. Porta nibh venenatis cras sed felis eget velit aliquet
+      sagittis.
+    </p>
+    <p>
+      At auctor urna nunc id cursus metus. Ac placerat vestibulum lectus mauris
+      ultrices. Elit sed vulputate mi sit amet mauris commodo quis. Vitae purus
+      faucibus ornare suspendisse sed. Orci dapibus ultrices in iaculis nunc.
+      Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.
+      At lectus urna duis convallis convallis tellus id. Sit amet est placerat
+      in. Consequat interdum varius sit amet mattis. Eget nunc scelerisque
+      viverra mauris. Ut eu sem integer vitae. Arcu bibendum at varius vel
+      pharetra vel turpis nunc. Ac odio tempor orci dapibus ultrices in. At
+      risus viverra adipiscing at. Ultrices in iaculis nunc sed augue lacus
+      viverra vitae. Vitae turpis massa sed elementum.
+    </p>
+    <p>
+      Tellus cras adipiscing enim eu turpis egestas. Euismod in pellentesque
+      massa placerat duis ultricies lacus. Id eu nisl nunc mi. Quam adipiscing
+      vitae proin sagittis nisl. Sed blandit libero volutpat sed cras. Ut eu sem
+      integer vitae justo eget magna fermentum. Fringilla urna porttitor rhoncus
+      dolor purus non enim praesent elementum. Orci phasellus egestas tellus
+      rutrum tellus pellentesque eu. Donec et odio pellentesque diam volutpat
+      commodo sed egestas egestas. Malesuada fames ac turpis egestas. Ac turpis
+      egestas sed tempus. Nibh cras pulvinar mattis nunc sed blandit libero
+      volutpat sed. Sit amet nisl suscipit adipiscing bibendum est ultricies
+      integer quis. Sit amet consectetur adipiscing elit ut aliquam purus. Ac
+      turpis egestas sed tempus urna. Vitae tortor condimentum lacinia quis vel.
+      Amet aliquam id diam maecenas ultricies mi eget mauris. Enim ut sem
+      viverra aliquet eget sit amet.
+    </p>
   </article>
 </template>
 
