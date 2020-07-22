940b17b56706ca5bd1ca3e0ed9b7b901d76c90d5
Tim Hinz <tim.hinz@guns.com>
2020-07-22 09:12:25 -0500

    Facebook shimmer loading effect

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <article v-if="1 == 2">
+    <article v-if="posts">
       <img
         v-if="posts.data.title_image.length > 0"
         :src="posts.data.title_image"
@@ -24,48 +24,24 @@
       </div>
     </article>
     <article v-else class="loading-container">
-      <svg
-        class="tea"
-        width="37"
-        height="48"
-        viewBox="0 0 37 48"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M27.0819 17H3.02508C1.91076 17 1.01376 17.9059 1.0485 19.0197C1.15761 22.5177 1.49703 29.7374 2.5 34C4.07125 40.6778 7.18553 44.8868 8.44856 46.3845C8.79051 46.79 9.29799 47 9.82843 47H20.0218C20.639 47 21.2193 46.7159 21.5659 46.2052C22.6765 44.5687 25.2312 40.4282 27.5 34C28.9757 29.8188 29.084 22.4043 29.0441 18.9156C29.0319 17.8436 28.1539 17 27.0819 17Z"
-          stroke="var(--secondary)"
-          stroke-width="2"
-        />
-        <path
-          d="M29 23.5C29 23.5 34.5 20.5 35.5 25.4999C36.0986 28.4926 34.2033 31.5383 32 32.8713C29.4555 34.4108 28 34 28 34"
-          stroke="var(--secondary)"
-          stroke-width="2"
-        />
-        <path
-          id="teabag"
-          fill="var(--secondary)"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M16 25V17H14V25H12C10.3431 25 9 26.3431 9 28V34C9 35.6569 10.3431 37 12 37H18C19.6569 37 21 35.6569 21 34V28C21 26.3431 19.6569 25 18 25H16ZM11 28C11 27.4477 11.4477 27 12 27H18C18.5523 27 19 27.4477 19 28V34C19 34.5523 18.5523 35 18 35H12C11.4477 35 11 34.5523 11 34V28Z"
-        />
-        <path
-          id="steamL"
-          d="M17 1C17 1 17 4.5 14 6.5C11 8.5 11 12 11 12"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke="var(--secondary)"
-        />
-        <path
-          id="steamR"
-          d="M21 6C21 6 21 8.22727 19 9.5C17 10.7727 17 13 17 13"
-          stroke="var(--secondary)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
+      <div class="post-wrapper">
+        <div class="placeload">
+          <div class="header">
+            <div class="header-content">
+              <div class="content-shape-large loads" />
+              <div class="content-shape-large-sub loads" />
+            </div>
+          </div>
+          <div class="image-placeholder loads" />
+          <div class="placeholder-footer">
+            <div class="footer-block">
+              <div class="content-shape loads" />
+              <div class="content-shape loads" />
+              <div class="content-shape loads" />
+            </div>
+          </div>
+        </div>
+      </div>
     </article>
   </div>
 </template>
@@ -116,148 +92,130 @@ article {
 }
 
 .loading-container {
-  min-height: 500px;
+  $background: #f6f7f8;
+  $grey-nuance-lighter: #eeeeee;
+  $grey-nuance-darker: #dddddd;
+  $fade-grey: #e8e8e8;
 
-  @-moz-keyframes swing {
-    50% {
-      transform: rotate(-3deg);
+  .header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    .img {
+      width: 50px;
+      min-width: 50px;
+      height: 50px;
+      border-radius: 50%;
+    }
+    .header-content {
+      width: 100%;
+      .content-shape-large {
+        height: 36px;
+        margin-bottom: 8px;
+        width: 50%;
+      }
+      .content-shape-large-sub {
+        height: 12px;
+        margin-bottom: 8px;
+        width: 30%;
+      }
+      .content-shape {
+        height: 8px;
+        margin-bottom: 8px;
+        &:first-child {
+          width: 80%;
+        }
+        &:nth-child(2) {
+          width: 60%;
+        }
+      }
     }
   }
-  @-webkit-keyframes swing {
-    50% {
-      transform: rotate(-3deg);
+  .image-placeholder {
+    width: 100%;
+    margin-top: 20px;
+    height: 250px;
+  }
+  .placeholder-footer {
+    position: relative;
+    margin-top: 20px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .footer-block {
+      width: 100%;
+      height: 100%;
+      min-height: 20px;
+      .content-shape {
+        height: 8px;
+        margin-bottom: 8px;
+        &:first-child {
+          width: 100%;
+        }
+        &:nth-child(2) {
+          width: 100%;
+        }
+        &:nth-child(3) {
+          width: 64%;
+        }
+      }
     }
   }
-  @-o-keyframes swing {
-    50% {
-      transform: rotate(-3deg);
-    }
+
+  .loads {
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
+    -webkit-animation-iteration-count: infinite;
+    animation-iteration-count: infinite;
+    -webkit-animation-name: placeload;
+    animation-name: placeload;
+    -webkit-animation-timing-function: linear;
+    animation-timing-function: linear;
+    background: $background;
+    background: $grey-nuance-lighter;
+    background: -webkit-gradient(
+      linear,
+      left top,
+      right top,
+      color-stop(8%, $grey-nuance-lighter),
+      color-stop(18%, $grey-nuance-darker),
+      color-stop(33%, $grey-nuance-lighter)
+    );
+    background: -webkit-linear-gradient(
+      left,
+      #eeeeee 8%,
+      #dddddd 18%,
+      #eeeeee 33%
+    );
+    background: linear-gradient(
+      to right,
+      $grey-nuance-lighter 8%,
+      $grey-nuance-darker 18%,
+      $grey-nuance-lighter 33%
+    );
+    -webkit-background-size: 800px 104px;
+    background-size: 1700px 104px;
+    position: relative;
   }
-  @keyframes swing {
-    50% {
-      transform: rotate(-3deg);
-    }
-  }
-  @-moz-keyframes steamLarge {
+
+  @-webkit-keyframes placeload {
     0% {
-      stroke-dashoffset: 13;
-      opacity: 0.6;
+      background-position: -468px 0;
     }
     100% {
-      stroke-dashoffset: 39;
-      opacity: 0;
+      background-position: 468px 0;
     }
   }
-  @-webkit-keyframes steamLarge {
+
+  @keyframes placeload {
     0% {
-      stroke-dashoffset: 13;
-      opacity: 0.6;
+      background-position: -868px 0;
     }
     100% {
-      stroke-dashoffset: 39;
-      opacity: 0;
-    }
-  }
-  @-o-keyframes steamLarge {
-    0% {
-      stroke-dashoffset: 13;
-      opacity: 0.6;
-    }
-    100% {
-      stroke-dashoffset: 39;
-      opacity: 0;
-    }
-  }
-  @keyframes steamLarge {
-    0% {
-      stroke-dashoffset: 13;
-      opacity: 0.6;
-    }
-    100% {
-      stroke-dashoffset: 39;
-      opacity: 0;
-    }
-  }
-  @-moz-keyframes steamSmall {
-    10% {
-      stroke-dashoffset: 9;
-      opacity: 0.6;
-    }
-    80% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-    100% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-  }
-  @-webkit-keyframes steamSmall {
-    10% {
-      stroke-dashoffset: 9;
-      opacity: 0.6;
-    }
-    80% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-    100% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-  }
-  @-o-keyframes steamSmall {
-    10% {
-      stroke-dashoffset: 9;
-      opacity: 0.6;
-    }
-    80% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-    100% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-  }
-  @keyframes steamSmall {
-    10% {
-      stroke-dashoffset: 9;
-      opacity: 0.6;
-    }
-    80% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-    100% {
-      stroke-dashoffset: 27;
-      opacity: 0;
-    }
-  }
-
-  svg.tea {
-    position: absolute;
-    --secondary: #33406f;
-    display: block;
-    left: calc(50% - 18px);
-    top: calc(50% - 24px);
-
-    #teabag {
-      transform-origin: top center;
-      transform: rotate(3deg);
-      animation: swing 2s infinite;
-    }
-
-    #steamL {
-      stroke-dasharray: 13;
-      stroke-dashoffset: 13;
-      animation: steamLarge 2s infinite;
-    }
-
-    #steamR {
-      stroke-dasharray: 9;
-      stroke-dashoffset: 9;
-      animation: steamSmall 2s infinite;
+      background-position: 868px 0;
     }
   }
 }
